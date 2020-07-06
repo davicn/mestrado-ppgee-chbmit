4204e6dac43ce20d67b64ce403eaec21a51a01bd
Davi Nascimento <davi.nascimento777@gmail.com>
2020-07-05 21:55:46 -0300

    Inicio de captura de janelas

---
+++ src/getInteval.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+from os.path import expanduser
+from mne.io import concatenate_raws, read_raw_edf
+
+HOME = expanduser("~")
+CHBMIT = expanduser('/home/davi/physionet.org/files/chbmit/1.0.0/')
+PATH = HOME + '/Github/mestrado-ppgee-chbmit/docs'
+
+RECORDS = pd.read_csv(PATH+'/RECORDS-WITH-SEIZURES', sep=" ")
+
+FILES = pd.read_json(PATH+'/seizures.json')
+
+x = read_raw_edf(CHBMIT+FILES.iloc[0, 0], preload=True).to_data_frame().T
+
+inicio =FILES.loc[0, 'Seizures'][0]['Start']
+fim = FILES.loc[0, 'Seizures'][0]['End']
+
+print(x.iloc[:, inicio:fim])
+
